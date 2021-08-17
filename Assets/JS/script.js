43153cb22bfe9959b8b7a38e945a4522ce5203ae
Alfonso Robles <fons3517@gmail.com>
2021-08-17 14:39:11 -0500

    Finished up buttons on HTML page. Moved to css and began styling the page.

---
+++ Assets/JS/script.js
@@ -0,0 +1,11 @@
+// Creating a timed quiz on JavaScript fundamentals that stores high scores
+
+// When I open the page, I see the title of the code quiz at the top
+// make html page and  css page and style html using css
+// I see there is a Start Game Button that needs to be pressed to start the game
+// A timer starts as soon as I click Start Game Button and I'm presented with a question
+// When I answer the question, I am presented with another question
+// When I answer incorrectly, time is subtracted from the clock
+// When all the questions are answered or the timer reaches 0, the game is over
+// When the game is over, I can save my initials and my score
+
