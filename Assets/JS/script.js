f4c9f4c8731d00762b2e5f28e9f8d83ea7846b73
Alfonso Robles <fons3517@gmail.com>
2021-08-18 00:47:12 -0500

    Created list of questions with answers respectively for each question.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,5 @@
+// Create a timed quiz on JavaScript fundamentals that stores high scores
+
 // Get a handle on html elements
 
 var container = document.querySelector('.container');
@@ -12,7 +14,48 @@ var btnNextEl = document.getElementById('next');
 var btnSubmitEl = document.getElementById('submit');
 
 
-// Creating a timed quiz on JavaScript fundamentals that stores high scores
+// Creating Questions and Answers Options
+
+var questions = [
+    {
+        question: " What is a call-back function?",
+        answers: [
+            {option:" A function passed as an arguement to another function.",answer:true},
+            {option:" A function with no name.",answer:false}
+        ]
+    },
+    {
+        question:" What is a modal?",
+        answers: [
+            {option:" The window to the DOM.",answer:false},
+            {option:" A pop-up screen that requires some form action before continuing.",answer:true}
+        ]
+    },
+    {
+        question:" Event Delegation is...",
+        answers: [
+            {option:" A method to get a handle on elements.",answer:false},
+            {option:" A pattern to handle events efficiently.",answer:true}
+        ]
+    },
+    {
+        question:" What is the purpose of a for-loop?",
+        answers: [
+            {option:" To exctract the value of an object.",answer:false},
+            {option:" To execute the same code more than once.",answer:true}
+        ]
+    },
+    {
+        question:" .unshift() adds element(s) to what part of an array?",
+        answers: [
+            {option:" To the beginning of an array.",answer:true},
+            {option:" Always after the second array.",answer:false}
+        ]
+    }
+
+];
+
+// Add onclick events to buttons
 
 // When I open the page, I see the title of the code quiz at the top
 // make html page and  css page and style html using css
@@ -22,4 +65,48 @@ var btnSubmitEl = document.getElementById('submit');
 // When I answer incorrectly, time is subtracted from the clock
 // When all the questions are answered or the timer reaches 0, the game is over
 // When the game is over, I can save my initials and my score
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function countdown() {
+    var timeRemaining = 180;
+
+    var timeInterval = setInterval(function () {
+
+        //seconds on screen
+        timeRemaining--;
+        timerEl.textContent = timeRemaining + ": Till your time expires!";
+        if(timeRemaining === 0) {
+            clearInterval(timeInterval);
+            displayMessage();
+        }
+    },1000);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
