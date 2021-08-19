febc7eb9e5a5ca19b1ebd171b1a30cb9b5fc2b78
Alfonso Robles <fons3517@gmail.com>
2021-08-19 14:00:58 -0500

    Created beginCodeiQuiz function, added eventListeners to buttons, reorganized pseudo-code.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,10 +1,10 @@
 // Create a timed quiz on JavaScript fundamentals that stores high scores
 
-// Get a handle on html elements
+// Get a handle on HTML elements
 
-var container = document.querySelector('.container');
+var containerEl = document.querySelector('.container');
+var scoreEl = document.querySelector('score');
 var userScoreEl = document.getElementById('user-score');
-var totalScoreEl = document.getElementById('total-score');
 var questionTextEl = document.getElementById('question-text');
 var btnTrueEl = document.getElementById('true');
 var btnFalseEl = document.getElementById('false');
@@ -14,7 +14,7 @@ var btnNextEl = document.getElementById('next');
 var btnSubmitEl = document.getElementById('submit');
 
 
-// Creating Questions and Answers Options
+// Creating Questions and Answers Options for Code iQuiz 101
 
 var questions = [
     {
@@ -55,32 +55,63 @@ var questions = [
 
 ];
 
-// Add onclick events to buttons
+
+
 
 // When I open the page, I see the title of the code quiz at the top
+    function beginCodeiQuiz() {
+        var currentQuestion = 0;
+        questionTextEl.innerHTML = questions[currentQuestion].question;
+        btnTrueEl.innerHTML = questions[currentQuestion].answers[0].option;
+        btnTrueEl.onclick = () => {
+            var choice = 0;
+            if(quesitons[currentQuestion].answers[choice].answer) {
+                if(score < 4) {
+                    scrore++;
+                };
+            };
+            userScoreEl.innerHTML = score;
+            if (currentQuestion < 4) {
+                next();
+            };
+        };
+        btnFalseEl.innerHTML = questions[currentQuestion].ansers[1].option;
+        btnFalseEl.onclick = () => {
+            var choice = 1;
+            if(questions[currentQuestion].answers[choice].answer) {
+                if(score < 4) {
+                    score++;
+                };
+            };
+            userScoreEl.innerHTML = score;
+            if(currentQuestion < 4) {
+                next();
+            };
+        };
+        btnPrevEl.classList.add('hide');
+    }
+
+    beginCodeiQuiz();
+
+    //Create restart function
+    function restart() {
+        currentQuestion = 0;
+        btnPrevEl.classList.remove('hide');
+        btnNextEl.classList.remove('hide');
+        btnSubmitEl.classList.remove('hide');
+        btnTrueEl.classList.remove('hide');
+        btnFalseEl.classList.remove('hide');
+        score = 0;
+        userScoreEl.innerHTML = score;
+        beginCodeiQuiz();
+    };
+    
+
+
+
 // make html page and  css page and style html using css
 // I see there is a Start Game Button that needs to be pressed to start the game
 // A timer starts as soon as I click Start Game Button and I'm presented with a question
-// When I answer the question, I am presented with another question
-// When I answer incorrectly, time is subtracted from the clock
-// When all the questions are answered or the timer reaches 0, the game is over
-// When the game is over, I can save my initials and my score
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function countdown() {
     var timeRemaining = 180;
 
@@ -94,7 +125,44 @@ function countdown() {
             displayMessage();
         }
     },1000);
+
+    function displayMessage() {
+        var message = "Time's up!";
+        if(timeRemaining === 0) {
+            return message;
+        };
+    
+    };
 };
+
+
+
+// When I answer the question, I am presented with another question
+// When I answer incorrectly, time is subtracted from the clock
+// When all the questions are answered or the timer reaches 0, the game is over
+// When the game is over, I can save my initials and my score
+
+
+
+
+// Add onclick events to buttons
+//btnTrueEl.addEventListener('click', true);
+btnRestartEl.addEventListener('click', restart);
+btnPrevEl.addEventListener('click', prev);
+btnNextEl.addEventListener('click', next);
+btnSubmitEl.addEventListener('click', submit);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
