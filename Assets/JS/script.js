6f6cac6f57d1e0d57a8ed0423cfaa0ad9cbb65fc
Alfonso Robles <fons3517@gmail.com>
2021-08-19 19:04:21 -0500

    Rearranged code and debugged

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,9 +1,8 @@
 // Create a timed quiz on JavaScript fundamentals that stores high scores
 
 // Get a handle on HTML elements
-
 var containerEl = document.querySelector('.container');
-var scoreEl = document.querySelector('score');
+var scoreEl = document.querySelector('.score');
 var userScoreEl = document.getElementById('user-score');
 var questionTextEl = document.getElementById('question-text');
 var btnTrueEl = document.getElementById('true');
@@ -12,14 +11,21 @@ var btnRestartEl = document.getElementById('restart');
 var btnPrevEl = document.getElementById('prev');
 var btnNextEl = document.getElementById('next');
 var btnSubmitEl = document.getElementById('submit');
-var startBtnEl = document.createElement('button')
+
+//Dynamically create h1 element;create countdown div handle; appenChild
+var startCountdownEl = document.querySelector('.start-countdown');
+var countdownTimerEl = document.createElement("h3");
+startCountdownEl.appendChild(countdownTimerEl);
 
 
-// Creating Questions and Answers Options for Code iQuiz 101
 
+    // Creating Questions and Answers Options for Code iQuiz 101
 var questions = [
     {
+        //questions first
         question: " What is a call-back function?",
+        
+        //answer options next
         answers: [
             {option:" A function passed as an arguement to another function.",answer:true},
             {option:" A function with no name.",answer:false}
@@ -58,104 +64,168 @@ var questions = [
 
 
 // Add onclick events to buttons
-//btnTrueEl.addEventListener('click', true);
 btnRestartEl.addEventListener('click', Restart);
 btnPrevEl.addEventListener('click', Prev);
 btnNextEl.addEventListener('click', Next);
 btnSubmitEl.addEventListener('click', Submit);
+startBtnEl.addEventListener('click', clickedStart);
 
 
 // I see there is a Start Game Button that needs to be pressed to start the game
-
-
-
-// When I open the page, I see the title of the code quiz at the top
-// When I answer the question, I am presented with another question
-
-    function beginCodeiQuiz() {
-        var currentQuestion = 0;
-        questionTextEl.innerHTML = questions[currentQuestion].question;
-        btnTrueEl.innerHTML = questions[currentQuestion].answers[0].option;
-        btnTrueEl.onclick = () => {
-            var choice = 0;
-            if(quesitons[currentQuestion].answers[choice].answer) {
-                if(score < 4) {
-                    scrore++;
-                };
-            };
-            userScoreEl.innerHTML = score;
-            if (currentQuestion < 4) {
-                next();
-            };
-        };
-        btnFalseEl.innerHTML = questions[currentQuestion].answers[1].option;
-        btnFalseEl.onclick = () => {
-            var choice = 1;
-            if(questions[currentQuestion].answers[choice].answer) {
-                if(score < 4) {
-                    score++;
-                };
-            };
-            userScoreEl.innerHTML = score;
-            if(currentQuestion < 4) {
-                next();
-            };
-        };
-        btnPrevEl.classList.add('hide');
-    }
-
-    //Creating Prev function to navigate from current page to previous page.
-    function Prev() {
-        currentQuestion--;
-
-    }
-
-    beginCodeiQuiz();
-
-    //Create restart function
-    function restart() {
-        currentQuestion = 0;
-        btnRestartEl.classList.remove('hide');
-        btnPrevEl.classList.remove('hide');
-        btnNextEl.classList.remove('hide');
-        btnSubmitEl.classList.remove('hide');
-        btnTrueEl.classList.remove('hide');
-        btnFalseEl.classList.remove('hide');
-        score = 0;
-        userScoreEl.innerHTML = score;
+function clickedStart() {
+        countdown();
         beginCodeiQuiz();
-    };
-
-
-
-
-// make html page and  css page and style html using css
-
-// A timer starts as soon as I click Start Game Button and I'm presented with a question
-function countdown() {
-    var timeRemaining = 180;
-
-    var timeInterval = setInterval(function () {
-
-        //seconds on screen
-        timeRemaining--;
-        timerEl.textContent = timeRemaining + ": Till your time expires!";
-        if(timeRemaining === 0) {
-            clearInterval(timeInterval);
-            displayMessage();
-        }
-    },1000);
-
-    function displayMessage() {
-        var message = "Time's up!";
-        if(timeRemaining === 0) {
-            return message;
-        };
-    
-    };
 };
 
 
+// Create countdown timer
+function countdown() {
+    var timeRemaining = 240;
+    var timeInterval = setInterval(function () {
+      timeRemaining--;
+      countdownTimerEl.textContent + ":Time Remaining.";
+      if(timeRemaining === 0) {
+        clearInterval(timeInterval);
+        return"Time's up!";
+      }
+    },1000);
+};
+
+
+
+// Create function for beginning code quiz
+function beginCodeiQuiz() {
+    var currentQuestion = 0;
+    questionTextEl.innerHTML = questions[currentQuestion].question;
+    btnTrueEl.innerHTML = questions[currentQuestion].answers[0].option;
+    btnTrueEl.onclick = () => {
+        var choice = 0;
+        if(quesitons[currentQuestion].answers[choice].answer) {
+            if(score < 4) {
+                scrore++;
+            };
+        };
+        userScoreEl.innerHTML = score;
+        if (currentQuestion < 4) {
+            next();
+        };
+    };
+    btnFalseEl.innerHTML = questions[currentQuestion].answers[1].option;
+    btnFalseEl.onclick = () => {
+        var choice = 1;
+        if(questions[currentQuestion].answers[choice].answer) {
+            if(score < 4) {
+                score++;
+            };
+        };
+        userScoreEl.innerHTML = score;
+        if(currentQuestion < 4) {
+            next();
+        };
+    };
+    btnPrevEl.classList.add('hide');
+};
+beginCodeiQuiz();
+
+//Create restart function
+function Restart() {
+    currentQuestion = 0;
+    startBtnEl.classList.remove('hide');
+    btnRestartEl.classList.remove('hide');
+    btnPrevEl.classList.remove('hide');
+    btnNextEl.classList.remove('hide');
+    btnSubmitEl.classList.remove('hide');
+    btnTrueEl.classList.remove('hide');
+    btnFalseEl.classList.remove('hide');
+    score = 0;
+    userScoreEl.innerHTML = score;
+    beginCodeiQuiz();
+};
+
+//Create Next function for button to navigate from current page to next page
+function Next() {
+    currentQuestion++;
+    if(currentQuestion >= 4) {
+        btnNextEl.classList.add('hide');
+        btnPrevEl.classList.remove('hide');
+    }
+    questionTextEl.innerHTML = questions[currentQuestion].quesiton;
+    btnTrueEl.innerHTML = quesiton[currentQuestion].answers[0].option;
+    btnTrueEl.onclick = () => {
+        var choice = 0;
+        if(quesiton[currentQuestion].answers[choice].answer) {
+            if(score < 4) {
+                score++;
+            };
+        };
+        userScoreEl.innerHTML = score;
+        if(currentQuestion < 2) {
+            next()
+        };
+    }
+    btnFalseEl.innerHTML = questions[currentQuestion].answers[1].option;
+    btnFalseEl.onclick = () => {
+        var choice = 1;
+        if(question[currentQuestion].answers[choice].answer) {
+            if(score < 4) {
+                score++;
+            };
+        };
+        userScoreEl.innerHTML = score;
+        if(currentQuestion < 2) {
+            next();
+        };
+    }
+    btnPrevEl.classList.remove('hide')
+};
+
+
+//Creating Prev function for button to navigate from current page to previous page.
+function Prev() {
+    currentQuestion--;
+    if(currentQuestion <= 0) {
+        btnPrevEl.classList.add('hide');
+        btnNextEl.classList.remove('hide');
+    };
+    questionTextEl.innerHTML = questions[currentQuestion].question;
+    btnTrueEl.innerHTML = questions[currentQuestion].answers[0].option;
+    btnTrueEl.onclick = () => {
+        var choice = 0;
+        if(questions.currentQuestion.answers[choice].answer) {
+            if(score < 5) {
+                score++;
+            };
+        }    
+        userScoreEl.innerHTML = score;
+        if(currentQuestion < 4) {
+            next();
+        }
+    };
+    btnFalseEl.innerHTML  = questions[currentQuestion].answers[1].option;
+    btnFalseEl.onclick = () => {
+        var choice = 1;
+        if(question[currentQuestion].answers[choice].answer) {
+            if(score < 5) {
+                score++;
+            }
+        }
+        userScoreEl.innerHTML = score;
+        if(currentQuestion < 2) {
+            next();
+        }
+    }
+    btnNextEl.classList.remove('hide');
+};
+
+//Creating submit function for button
+function Submit() {
+    btnSubmitEl.classList.add('hide');
+    btnTrueEl.classList.add('hide');
+    btnFalseEl.classList.add('hide');
+    btnPrevEl.classList.add('hide');
+    btnNextEl.classList.add('hide');
+    questionTextEl.innerHTML = "Way to go! Let's see how you did!!";
+};
 
 // When I answer incorrectly, time is subtracted from the clock
 // When all the questions are answered or the timer reaches 0, the game is over
